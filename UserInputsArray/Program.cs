300871f9f2077b0c00d418e8aa34e81ad2f62606
Anna Hanoshenko <hanoshenkoanna@gmail.com>
2025-03-18 16:41:13 +0100

    Add project files.

---
+++ UserInputsArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] numbers = new int[5];
+Console.WriteLine("Enter 5 integers");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("Enter number {0}: ", i + 1);
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+    Console.WriteLine("User input finished. Processing...");
+
+foreach(int num in numbers)
+{
+    Console.Write((num * 10) + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
